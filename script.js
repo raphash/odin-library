f140d1f4ebc97acca943c5327ce2128b351196fc
raphash <175216103+raphash@users.noreply.github.com>
2025-01-03 15:57:57 -0300

    refactor: Change constructor function to class

--- script.js
+++ script.js
@@ -12,12 +12,14 @@ const dialogInfo = {
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+};
 
 function addBookToLibrary(book) {
   for (const libBook of myLibrary) {
